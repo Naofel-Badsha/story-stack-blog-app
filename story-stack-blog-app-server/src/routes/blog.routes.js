e3f375b995a4e7f49a8b9da2d246287dbcebc443
Naofel-Badsha <naofelbadsha@gmail.com>
2026-05-12 22:13:17 +0600

    added find single blog

--- story-stack-blog-app-server/src/routes/blog.routes.js
+++ story-stack-blog-app-server/src/routes/blog.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const Blog = require("../models/blog.model");
+const { ObjectId } = require("mongodb");
 const router = express.Router()
 
 //---------Http Post Method----------
-//---------Get all blog-----------
-router.get('/', async(req, res) => {
+//---------Get all Blogs-----------
+router.get('/', async (req, res) => {
     try {
-        const blogs = await Blog.find()
+        const blogs = await Blog.find().sort({ createdAt: -1 })
         res.status(201).json({
             success: true,
             message: "views All Blogs successfully",
@@ -23,8 +24,40 @@ router.get('/', async(req, res) => {
 })
 
 
+
 //---------Http Post Method----------
-//---------Post a new blog-----------
+//---------Get a Single Blog-----------
+router.get("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const blog = await Blog.findById(id)
+        //---------If don't find blog--------
+        if (!blog) {
+            return res.status(404).json({
+                success: false,
+                message: "Blog not found"
+            });
+        }
+        res.status(201).json({
+            success: true,
+            message: "views Single Blogs successfully",
+            blog
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to Single blogs",
+            error: error.message
+        });
+    }
+
+})
+
+
+
+
+//---------Http Post Method----------
+//---------Post a new Blog-----------
 router.post("/add-post", async (req, res) => {
     try {
         const newBlog = new Blog({
