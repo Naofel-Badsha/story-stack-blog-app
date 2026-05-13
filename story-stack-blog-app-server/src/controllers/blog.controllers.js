4cb9fe18e1c1aafc766363def24dda6c19409a0a
Naofel-Badsha <naofelbadsha@gmail.com>
2026-05-14 00:09:27 +0600

    added fetching data form backend to addde fornt

--- story-stack-blog-app-server/src/controllers/blog.controllers.js
+++ story-stack-blog-app-server/src/controllers/blog.controllers.js
@@ -102,10 +102,38 @@ const updatedABlog = async (req, res) => {
 
 
 
+//---------Delete a Single Blog-----------
+const deleteASingleBlog = async (req, res) => {
+    try {
+        const { id } = req.params
+        const blog = await Blog.findByIdAndDelete(id)
+        //---------If don't find blog--------
+        if (!blog) {
+            return res.status(404).json({
+                success: false,
+                message: "Blog not found"
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: "Deleted Single Blog successfully",
+            blog
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Deleted Single Blog Failed",
+            error: error.message
+        });
+    }
+}
+
+
 
 module.exports = {
     getAllBlogs,
     GetASingleBlog,
     addedANewBlog,
-    updatedABlog
+    updatedABlog,
+    deleteASingleBlog 
 }
--- story-stack-blog-app-server/src/routes/blog.routes.js
+++ story-stack-blog-app-server/src/routes/blog.routes.js
@@ -1,60 +1,24 @@
 const express = require("express");
 const Blog = require("../models/blog.model");
 const { ObjectId } = require("mongodb");
-const { getAllBlogs, GetASingleBlog, addedANewBlog, updatedABlog } = require("../controllers/blog.controllers");
+const { getAllBlogs, GetASingleBlog, addedANewBlog, updatedABlog, deleteASingleBlog } = require("../controllers/blog.controllers");
 const router = express.Router()
 
-//---------Http Get Method----------
-//---------Get all Blogs-----------
+
+//---------Http Get all Blogs-----------
 router.get('/', getAllBlogs)
 
-
-
-//---------Http Get/:id Method----------
-//---------Get a Single Blog-----------
+//---------Http Get a Single Blog-----------
 router.get("/:id", GetASingleBlog)
 
-
-
-//---------Http Post Method----------
-//---------Post a new Blog-----------
+//---------Http Post a new Blog-----------
 router.post("/add-post", addedANewBlog);
 
-
-
-//---------Http Put meain Update Method----------
 //---------Put a new Blog-----------
 router.put("/:id", updatedABlog)
 
-
-
-//---------Http Delete Method----------
-//---------Delete a Single Blog-----------
-router.delete("/:id", async (req, res) => {
-    try {
-        const { id } = req.params
-        const blog = await Blog.findByIdAndDelete(id)
-        //---------If don't find blog--------
-        if (!blog) {
-            return res.status(404).json({
-                success: false,
-                message: "Blog not found"
-            });
-        }
-        res.status(200).json({
-            success: true,
-            message: "Deleted Single Blog successfully",
-            blog
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "Deleted Single Blog Failed",
-            error: error.message
-        });
-    }
-})
-
+//---------Http Delete a Single Blog-----------
+router.delete("/:id", deleteASingleBlog)
 
 
 module.exports = router
